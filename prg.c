41080261e73d9852e91b6eb5a6b1408ba782d078
Tomasz Smogór <tsmogor@p29-9.labpk.inf.ug.edu.pl>
2012-11-27 16:44:57 +0100

    Prosty program dodajacy

---
+++ prg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+  int o;
+  int b;
+
+  printf("\nPodaj 2 cyfry : ");
+
+  scanf("%d",&o);
+scanf("%d",&b);
+
+ printf("\nWynik dodawania to %d",o+b);
+ printf("\n\n");
+}
+
+  
