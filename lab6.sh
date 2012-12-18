76119fb8f18376860ea2f9298a638ed615cbd611
Tomasz Smogór <tsmogor@p29-9.labpk.inf.ug.edu.pl>
2012-12-18 16:08:18 +0100

    Lab06

---
+++ lab6.sh
@@ -0,0 +1,17 @@
+# punkt 1
+grep . plik.txt
+
+# punkt 2
+grep -n '^[0-9]' pl*
+
+# punkt 3
+grep -n '........r' *
+
+# punkt 4
+grep bash$ passwd
+
+# punkt 5
+grep '[IVXLCDM]' plik.txt 
+
+egrep 'I{1,3}V{1}L{1}X{1,3}C{1,3}D{1}M{1,}' w.txt 
+
